071a70417aaa12162e767bc009ca9a9a30c9266f
samuelsenerwa <samuelsenerwa@gmail.com>
2024-10-14 00:34:48 +0300

    added gitignore && renamed files

--- composite-data.py
+++ composite-data.py
@@ -1,6 +1,8 @@
 import csv
 import copy
 
+
+
 myVehicle = {
     "vin" : "<empty>",
     "make" : "<empty>" ,
@@ -20,13 +22,16 @@ myInventoryList = []
 with open('car_fleet.csv') as csvFile:
     csvReader = csv.reader(csvFile, delimiter=',')  
     lineCount = 0  
+
     for row in csvReader:
         if lineCount == 0:
             print(f'Column names are: {", ".join(row)}')  
             lineCount += 1  
         else:  
             print(f'vin: {row[0]} make: {row[1]}, model: {row[2]}, year: {row[3]}, range: {row[4]}, topSpeed: {row[5]}, zeroSixty: {row[6]}, mileage: {row[7]}')  
+
             currentVehicle = copy.deepcopy(myVehicle)  
+            
             currentVehicle["vin"] = row[0]  
             currentVehicle["make"] = row[1]  
             currentVehicle["model"] = row[2]  
